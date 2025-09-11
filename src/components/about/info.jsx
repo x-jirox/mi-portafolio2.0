5e6acc17351399c9fa8b0d37f15c9d57be9d4fe2
x-jirox <correalenin045@gmail.com>
2025-09-10 20:02:48 -0500

    design about section

---
+++ src/components/about/info.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Info = () => {
+  return (
+    <div className="about__info grid">
+      <div className="about__box">
+        <i class="bx  bx-medal-alt about__icon"></i>
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">1 Year Working whit freelancer </span>
+      </div>
+
+      <div className="about__box">
+        <i class="bx bx-briefcase-alt about__icon"></i>
+        <h3 className="about__title">Completed</h3>
+        <span className="about__subtitle">5 + Projects</span>
+      </div>
+
+      <div className="about__box">
+        <i class="bx  bx-headphone-mic about__icon"></i>
+        <h3 className="about__title">Support</h3>
+        <span className="about__subtitle">Online 24/7</span>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
