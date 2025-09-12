41d99b12496c3eace2ff1f9e5defd426bbe5a416
x-jirox <correalenin045@gmail.com>
2025-09-11 19:48:35 -0500

    component skills

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/header/header';
 import Home from './components/home/home';
 import About from './components/about/about';
+import Skills from './components/skills/skills';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <main className='main'>
               <Home/>
               <About/>
+              <Skills/>
       </main>
     </>
   );
