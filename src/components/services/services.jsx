a16cf4ca4bd5294f8afa2f24a2bf460a24c89927
x-jirox <correalenin045@gmail.com>
2025-09-16 20:40:12 -0500

    design services

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -6,25 +6,148 @@ const Services = () => {
       <h2 className="section__title">Services</h2>
       <span className="section__subtitle">What i offer</span>
 
-      <div className="services__container container geid">
+      <div className="services__container container grid">
         <div className="services__content">
           <div>
-            <i class="uil uil services__icon"></i>
-            <h3 className="services__title"></h3>
+            <i class="uil uil-web-grid services__icon"></i>
+            <h3 className="services__title">
+              Product <br /> Design
+            </h3>
           </div>
           <span className="services__button">
-            View Mpre
+            View More
             <i className="uil uil-arrow-right services__button-icon"></i>
           </span>
           <div className="services__modal">
             <div className="services__modal-content">
               <i className="uil uil-times services__modal-close"></i>
-              <h3 className="services__modal-tittle"></h3>
+              <h3 className="services__modal-tittle">Product Design</h3>
               <p className="services__modal-sescription">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Quisquam, quod.
               </p>
               <ul className="services__modal-services grid">
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle service__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop the user interface.
+                  </p>
+                </li>
+
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle service__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop the user interface.
+                  </p>
+                </li>
+
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle service__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop the user interface.
+                  </p>
+                </li>
+
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle service__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop the user interface.
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="services__content">
+          <div>
+            <i class="uil uil-arrow services__icon"></i>
+            <h3 className="services__title">Ui/Ux <br/>Design</h3>
+          </div>
+          <span className="services__button">
+            View More
+            <i className="uil uil-arrow-right services__button-icon"></i>
+          </span>
+          <div className="services__modal">
+            <div className="services__modal-content">
+              <i className="uil uil-times services__modal-close"></i>
+              <h3 className="services__modal-tittle">Ui/Ux Deesign</h3>
+              <p className="services__modal-sescription">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam, quod.
+              </p>
+              <ul className="services__modal-services grid">
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle service__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop the user interface.
+                  </p>
+                </li>
+
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle service__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop the user interface.
+                  </p>
+                </li>
+
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle service__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop the user interface.
+                  </p>
+                </li>
+
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle service__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop the user interface.
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="services__content">
+          <div>
+            <i class="uil uil-edit services__icon"></i>
+            <h3 className="services__title">Visual <br/>Design</h3>
+          </div>
+          <span className="services__button">
+            View More
+            <i className="uil uil-arrow-right services__button-icon"></i>
+          </span>
+          <div className="services__modal">
+            <div className="services__modal-content">
+              <i className="uil uil-times services__modal-close"></i>
+              <h3 className="services__modal-tittle">Visual Designer</h3>
+              <p className="services__modal-sescription">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam, quod.
+              </p>
+              <ul className="services__modal-services grid">
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle service__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop the user interface.
+                  </p>
+                </li>
+
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle service__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop the user interface.
+                  </p>
+                </li>
+
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle service__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop the user interface.
+                  </p>
+                </li>
+
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle service__modal-icon"></i>
                   <p className="services__modal-info">
