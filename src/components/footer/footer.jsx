01050fb7e75924b13ba75593b1abf6e8bb4e1f13
x-jirox <correalenin045@gmail.com>
2025-09-23 21:43:56 -0500

    component footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./footer.css"
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
+        <h1 className="footer__title">Lenin Correa</h1>
+
+        <ul className="footer__list">
+          <li>
+            <a href="#about" className="footer__link">About</a>
+          </li>
+          <li>
+            <a href="#portfolio" className="footer__link">Projects</a>
+          </li>
+
+        </ul>
+
+        <div className="footer__social">
+                  <a
+        href="www.linkedin.com/in/lenin-correa-b496b5272"
+        className="footer__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-linkedin"></i>
+      </a>
+      <a
+        href="https://github.com/x-jirox"
+        className="footer__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github"></i>
+      </a>
+      <a
+        href="https://www.instagram.com/ss.jiro/"
+        className="footer__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-instagram-alt"></i>
+      </a>
+        </div>
+        <span className="footer__copy">
+            &#169; All rigths reserved
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
