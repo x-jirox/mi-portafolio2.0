a2b2abd17944933f6573d25b311efcc2096c9f32
x-jirox <correalenin045@gmail.com>
2025-09-25 23:56:11 -0500

    component scroll

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./footer.css"
+import "./footer.css";
 
 const Footer = () => {
   return (
@@ -9,40 +9,41 @@ const Footer = () => {
 
         <ul className="footer__list">
           <li>
-            <a href="#about" className="footer__link">About</a>
+            <a href="#about" className="footer__link">
+              About
+            </a>
           </li>
           <li>
-            <a href="#portfolio" className="footer__link">Projects</a>
+            <a href="#portfolio" className="footer__link">
+              Projects
+            </a>
           </li>
-
         </ul>
 
         <div className="footer__social">
-                  <a
-        href="www.linkedin.com/in/lenin-correa-b496b5272"
-        className="footer__social-icon"
-        target="_blank"
-      >
-        <i class="uil uil-linkedin"></i>
-      </a>
-      <a
-        href="https://github.com/x-jirox"
-        className="footer__social-icon"
-        target="_blank"
-      >
-        <i class="uil uil-github"></i>
-      </a>
-      <a
-        href="https://www.instagram.com/ss.jiro/"
-        className="footer__social-icon"
-        target="_blank"
-      >
-        <i class="uil uil-instagram-alt"></i>
-      </a>
+          <a
+            href="www.linkedin.com/in/lenin-correa-b496b5272"
+            className="footer__social-link"
+            target="_blank"
+          >
+            <i class="uil uil-linkedin"></i>
+          </a>
+          <a
+            href="https://github.com/x-jirox"
+            className="footer__social-link"
+            target="_blank"
+          >
+            <i class="uil uil-github"></i>
+          </a>
+          <a
+            href="https://www.instagram.com/ss.jiro/"
+            className="footer__social-link"
+            target="_blank"
+          >
+            <i class="uil uil-instagram-alt"></i>
+          </a>
         </div>
-        <span className="footer__copy">
-            &#169; All rigths reserved
-        </span>
+        <span className="footer__copy">&#169; All rigths reserved</span>
       </div>
     </footer>
   );
---
+++ src/components/scrollup/scrollup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Scrollup = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Scrollup
