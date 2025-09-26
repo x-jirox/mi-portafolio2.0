95086e1604389c2bb9ddb52479d10aedc6786d3b
x-jirox <correalenin045@gmail.com>
2025-09-26 00:18:37 -0500

    finished scroll component

--- src/components/scrollup/scrollup.jsx
+++ src/components/scrollup/scrollup.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import "./scrollup.css";
 
 const Scrollup = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  window.addEventListener("scroll", function () {
+    const scrollUp = document.querySelector(".scrollup");
+    if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+    else scrollUp.classList.remove("show-scroll");
+  });
 
-export default Scrollup
+  return (
+    <a href="#" className="scrollup">
+      <i className="uil uil-arrow-up scrollup__icon"></i>
+    </a>
+  );
+};
+
+export default Scrollup;
